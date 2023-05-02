5dcef3b6c40ba2fca59f18ebcf7b5055ee6ab3b4
AaronOrtega1 <aaron.ortega15@outlook.es>
2023-05-02 13:42:23 -0600

    somethings

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
 const teachersRoute = require("./routes/teachers-route.js");
-//const groupsRoute = require("./routes/");
+const groupsRoute = require("./routes/groups-routes.js");
 const subjectsRoute = require("./routes/subjects-routes.js");
+const studentsRoute = require("./routes/students-routes.js");
 const { escape } = require("querystring");
 const port = process.env.PORT || 3000;
 const app = express();
@@ -14,5 +15,9 @@ app.use(express.json());
 app.use("/api/teachers", teachersRoute);
 
 app.use("/api/asignaturas", subjectsRoute);
+
+app.use("/api/students", studentsRoute);
+
+app.use("/api/groups", groupsRoute);
 
 app.listen(port, () => console.log("running on port" + port));
