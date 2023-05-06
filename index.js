2ef966a4ffe9f22032f5342eb51b1c03b422da35
Angel Cortes <is726442@iteso.mx>
2023-05-05 19:18:56 -0500

    Guarde los cambios locales antes de fucionar con el remoto

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const teachersRoute = require("./routes/teachers-route.js");
 const groupsRoute = require("./routes/groups-routes.js");
 const subjectsRoute = require("./routes/subjects-routes.js");
 const studentsRoute = require("./routes/students-routes.js");
+const coordinadorRoute = require('./routes/coordinador-route.js')
 const { escape } = require("querystring");
 const port = process.env.PORT || 3000;
 const app = express();
@@ -19,5 +20,7 @@ app.use("/api/asignaturas", subjectsRoute);
 app.use("/api/students", studentsRoute);
 
 app.use("/api/groups", groupsRoute);
+
+app.use('/api/coordinador', coordinadorRoute)
 
 app.listen(port, () => console.log("running on port" + port));
---
+++ routes/coordinador-route.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const { Teacher } = require("../db/teacher.js");
+// const {validateBodyTeacher} = require('../middleware/validateData.js');
+const nanoid = require("nanoid");
+
+router.post("/", async (req, res) =>{
+  if(req.query.fullName &&
+    req.query.department &&
+    req.query.birthDate &&
+    req.query.status &&
+    req.query.userName &&
+    req.query.passWord &&
+    req.query.isCoord
+    ){
+      let {fullName, department, birthDate, status, userName, passWord, isCoord} = req.query
+      let newProfessor = await Teacher.createTeacher({
+        teacherID: nanoid.nanoid(),
+        fullName,
+        department,
+        birthDate,
+        status,
+        userName,
+        passWord,
+        isCoord,
+      })
+      console.log('Profesor Creado');
+      res.status(201).send(newProfessor)
+    }
+    else{
+      res.send('Error faltan atributos')
+    }
+  })
+
+module.exports = router;
