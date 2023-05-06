a3325bb4db27cd2abee8a235fb4976ccff638e0b
Your Name <you@example.com>
2023-05-06 12:43:49 -0600

    update of the coordinator View

--- db/administrator-cord-view.js
+++ db/administrator-cord-view.js
@@ -20,3 +20,40 @@ const viewSchema = mongoose.Schema({
         required: true
     }
 });
+
+
+viewSchema.statics.getView = async(filters) => {
+    let view = await View.find(filters);
+    console.log("View: \n" + view);
+    return view;
+}
+
+viewSchema.statics.getViewById = async(id) => {
+    let view = await View.findOne({id});
+    console.log("View: \n" + view);
+    return view;
+}
+
+viewSchema.statics.createView = async(viewData) => {
+    let newView = await View(viewData);
+    return await newView.save();
+}
+
+viewSchema.statics.updateView = async(id, datos) => {
+    let viewUpdated = await View.findOneAndUpdate({id}, {$set: datos}, {new: true});
+    console.log("View updated: \n" + viewUpdated);
+    return viewUpdated;
+}
+
+viewSchema.statics.deleteView = async(id) => {
+    let viewDeleted = await View.findOneAndDelete({id});
+    console.log("View deleted: \n" + viewDeleted);
+    return viewDeleted;
+}
+
+
+
+let View = mongoose.model("View", viewSchema);
+module.exports = { View };
+
+
