620291a10f19c066b11c6b03b3c0713f7a36fc5c
DanvDev02 <dan.vazquez@iteso.mx>
2023-05-08 20:14:19 -0600

    Creation of the Schema for the evidences

---
+++ db/evidence.js
@@ -0,0 +1,64 @@
+const { mongoose } = require("./connectDB.js");
+
+const  evidenceSchema = mongoose.Schema({
+    codigo: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    titulo: {
+        type: String,
+        required: true
+    },
+    urlArchivo: {
+        type: String,
+        required: true
+    },
+    descripcion: {
+        type: String,
+        required: true
+    },
+    teacher: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Teacher",
+        required: true
+    }
+});
+
+evidenceSchema.statics.getEvidencias = async(filters, pagina, limite) => {
+    let salto = (pagina - 1) * limite;
+    let evidencias = await Evidence.find(filters).skip(salto).limit(limite);
+    console.log("All: \n" + evidencias);
+    return evidencias;
+}
+
+evidenceSchema.statics.getEvidenciaById = async(codigo) => {
+    let evidence = await Evidence.findOne({codigo});
+    console.log("Obtained Evidence: \n" + evidence);
+    return evidence;
+}
+
+evidenceSchema.statics.createEvidencia = async(evidenceData) => {
+    let nuevaEvidencia = await Evidence(evidenceData);
+    console.log("New Evidence: \n" + nuevaEvidencia);
+    return await nuevaEvidencia.save();
+}
+
+evidenceSchema.statics.updateEvidencia = async(codigo, evidenceData) => {
+    let updatedEvidence = await Evidence.findOneAndUpdate({codigo}, {$set: evidenceData}, {new: true});
+    console.log("Updated Evidence: \n" + updatedEvidence);
+    return updatedEvidence;
+}
+
+evidenceSchema.statics.deleteEvidencia = async(codigo) => {
+    let deletedEvidence = await Evidence.findOneAndDelete({codigo});
+    console.log("Deleted Evidence: \n" + deletedEvidence);
+    return deletedEvidence;
+}
+
+
+
+
+
+let Evidence = mongoose.model("evidence", evidenceSchema);
+module.exports = { Evidence };
