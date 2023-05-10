2cb3d3ff85c3bca8e23ad227d3fb0a6062ab588a
AaronOrtega1 <aaron.ortega15@outlook.es>
2023-05-09 18:06:05 -0600

    tried to connect to database

--- config/config.js
+++ config/config.js
@@ -1,11 +1,12 @@
 let dbConfig = {
   user: "cg727976",
-  password: "BlurryFace001",
-  dbName: 'taskDB',
-  dbURL: function(){
-    return `mongodb+srv://${this.user}:${this.password}@cluster0.vn6sprx.mongodb.net/${this.dbName}?retryWrites=true&w=majority`
+  password: "mongoEscuela123",
+  dbName: "taskDB",
+  dbURL: function () {
+    return `mongodb+srv://${this.user}:${this.password}@cluster0.vn6sprx.mongodb.net/${this.dbName}?retryWrites=true&w=majority
+    `;
   },
-  jwtSecret: "66646"
-}
+  jwtSecret: "66646",
+};
 
 module.exports = dbConfig;
