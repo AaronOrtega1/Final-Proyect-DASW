0f81dc19b90e1f893f1fddd8fd933342f96c69d0
AntonioRodriguez2211 <antonio.rodriguez@iteso.mx>
2023-05-10 09:34:55 -0600

    usage of my db (Tony)

--- config/config.js
+++ config/config.js
@@ -1,12 +1,11 @@
 let dbConfig = {
-  user: "cg727976",
-  password: "mongoEscuela123",
-  dbName: "taskDB",
-  dbURL: function () {
-    return `mongodb+srv://${this.user}:${this.password}@cluster0.vn6sprx.mongodb.net/${this.dbName}?retryWrites=true&w=majority
-    `;
+  user: "antoniorodriguez",
+  password: "ilmmcW22",
+  dbName: 'daswpfDB',
+  dbURL: function(){
+    return `mongodb+srv://${this.user}:${this.password}@cluster0.yqauqpu.mongodb.net/${this.dbName}?retryWrites=true&w=majority`
   },
-  jwtSecret: "66646",
+  jwtSecret: "66646"
 };
 
 module.exports = dbConfig;
