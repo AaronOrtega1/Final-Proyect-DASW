591eb805f000e5acbb06e3936d974f2a2d8ead68
AaronOrtega1 <aaron.ortega15@outlook.es>
2023-05-14 12:39:35 -0600

    start of the connection back and front end

--- JavaScript/user-profile.js
+++ JavaScript/user-profile.js
@@ -1,203 +1,198 @@
 let users = [];
 
-let userProfile = document.getElementById("containerProfile");
+let userProfile = document.getElementById("profileUser");
 
-async function loadGroups(queryParams = "") {
+async function loadUsers(queryParams = "") {
   let resp = await fetch("/api/users" + queryParams);
-  groups = await resp.json();
-  showGroups(groups);
+  users = await resp.json();
+  console.log(
+    "🚀 ~ file: user-profile.js:8 ~ loadUsers ~ resp.json():",
+    resp.json()
+  );
+
+  for (let i = 0; i < users.lenght; i++) {
+    if (users[i].userID === "5U0Tk4PkUhf_Z0CcdKHWp") {
+      let testUser = users[i];
+      console.log(
+        "🚀 ~ file: user-profile.js:16 ~ loadUsers ~ testUser:",
+        testUser
+      );
+    }
+    showProfile(testUser);
+  }
 }
 
-const currentUser = (user) => user.userID === userID;
+/* const currentUser = (user) => user.userID === userID; */
 
-function showProfile(currentUser) {
-  if (currentUser.isCoord) {
+function showProfile(user) {
+  if (user.isCoord && !user.isAdmin) {
     userProfile.innerHTML = /*HTML*/ `
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-    <li class="nav-item active">
-      <div class="form-group m-1">
-        <input
-          type="text"
-          class="form-control"
-          name="searchBar"
-          id="searchBar"
-          aria-describedby="helpId"
-          placeholder="Buscar elemento"
-        />
-      </div>
-    </li>
-    <li class="nav-item">
-      <div class="m-1">
-        <a
-          name="buscar"
-          id="buscar"
-          class="btn btn-dark border border-white"
-          href="#"
-          role="button"
-          >Buscar</a
-        >
-      </div>
-    </li>
-  </ul>
-  <div>
-    <a
-      name=""
-      id=""
-      class="btn btn-dark"
-      href="coordinador.html"
-      role="button"
-      ><i class="fa fa-book"></i> Registrar Prof</a
-    >
-  </div>
-  <div>
-    <a
-      name=""
-      id=""
-      class="btn btn-dark"
-      href="administracion_coordinadores.html"
-      role="button"
-      ><i class="fa fa-edit"></i> Coordinadores</a
-    >
-  </div>
-  <div>
-    <a
-      name=""
-      id=""
-      class="btn btn-dark"
-      href="asignaturas.html"
-      role="button"
-      ><i class="fas fa-list"></i> Asignaturas</a
-    >
-  </div>
-  <div>
-    <a
-      name=""
-      id=""
-      class="btn btn-dark"
-      href="profesorPerfil.html"
-      role="button"
-      ><i class="fas fa-user"></i
-    ></a>
-  </div>
-</div>
-</nav>
-
-<div class="container emp-profile" id="containerProfile">
-<form method="post">
-  <div class="row">
-    <div class="col-md-4">
-      <div class="profile-img">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog"
-          alt=""
-        />
-        <div class="file btn btn-lg btn-primary">
-          Change Photo
-          <input type="file" name="file" />
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="profile-head">
-        <h5>${currentUser.fullName}</h5>
-        <h6>Coordinador de ${currentUser.department}</h6>
-        <p class="proile-rating">RANKINGS : <span>8/10</span></p>
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-          <li class="nav-item">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <a class="navbar-brand" href="./profesorPerfil.html">DASW</a>
+    <button
+      class="navbar-toggler d-lg-none"
+      type="button"
+      data-toggle="collapse"
+      data-target="#collapsibleNavId"
+      aria-controls="collapsibleNavId"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    ></button>
+    <div class="collapse navbar-collapse" id="collapsibleNavId">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <div class="form-group m-1">
+            <input
+              type="text"
+              class="form-control"
+              name="searchBar"
+              id="searchBar"
+              aria-describedby="helpId"
+              placeholder="Buscar elemento"
+            />
+          </div>
+        </li>
+        <li class="nav-item">
+          <div class="m-1">
             <a
-              class="nav-link active"
-              id="home-tab"
-              data-toggle="tab"
-              href="#home"
-              role="tab"
-              aria-controls="home"
-              aria-selected="true"
-              >About</a
+              name="buscar"
+              id="buscar"
+              class="btn btn-dark border border-white"
+              href="#"
+              role="button"
+              >Buscar</a
             >
-          </li>
-          <!--                 <li class="nav-item">
-            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Timeline</a>
-          </li> -->
-        </ul>
-      </div>
-    </div>
-    <div class="col-md-2">
-      <input
-        type="submit"
-        class="profile-edit-btn"
-        name="btnAddMore"
-        value="Edit Profile"
-      />
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-4">
-      <div class="profile-work">
-        <p>WORK LINK</p>
-        <a href="./gruposCargo.html">Grupos a Cargo</a><br />
-        <p>Dar de alta o algo asi pero en ingles xd</p>
-        <a href="./actAsignar.html">Asignación de Profesores</a><br />
-      </div>
-    </div>
-    <div class="col-md-8">
-      <div class="tab-content profile-tab" id="myTabContent">
-        <div
-          class="tab-pane fade show active"
-          id="home profesorProfile"
-          role="tabpanel"
-          aria-labelledby="home-tab"
+          </div>
+        </li>
+      </ul>
+      <div>
+        <a
+          name=""
+          id=""
+          class="btn btn-dark"
+          href="asignaturas.html"
+          role="button"
+          ><i class="fas fa-list"></i> Asignaturas</a
         >
-          <div class="row">
-            <div class="col-md-6">
-              <label>User Id</label>
-            </div>
-            <div class="col-md-6">
-              <p>Kshiti123</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6">
-              <label>Name</label>
-            </div>
-            <div class="col-md-6">
-              <p>Kshiti Ghelani</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6">
-              <label>Email</label>
-            </div>
-            <div class="col-md-6">
-              <p>kshitighelani@iteso.mx</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6">
-              <label>Phone</label>
-            </div>
-            <div class="col-md-6">
-              <p>123 456 7890</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-6">
-              <label>Profession</label>
-            </div>
-            <div class="col-md-6">
-              <p>Coordinadora de Equis Materia</p>
-            </div>
-          </div>
-        </div>
-
+      </div>
+      <div>
+        <a
+          name=""
+          id=""
+          class="btn btn-dark"
+          href="profesorPerfil.html"
+          role="button"
+          ><i class="fas fa-user"></i
+        ></a>
       </div>
     </div>
+  </nav>
+  
+  <div class="container emp-profile" id="containerProfile">
+  <form method="post">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="profile-img">
+          <img
+            src="user.imgURL"
+            alt="Profile Picture"
+          />
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="profile-head">
+          <h5>${user.fullName}</h5>
+          <h6>Coordinador de ${user.department}</h6>
+          <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                id="home-tab"
+                data-toggle="tab"
+                href="#home"
+                role="tab"
+                aria-controls="home"
+                aria-selected="true"
+                >About</a
+              >
+            </li>
+            <!--                 <li class="nav-item">
+              <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Timeline</a>
+            </li> -->
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-2">
+        <input
+          type="submit"
+          class="profile-edit-btn"
+          name="btnAddMore"
+          value="Edit Profile"
+        />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="profile-work">
+          <p>WORK LINK</p>
+          <a href="./gruposCargo.html">Grupos a Cargo</a><br />
+          <p>Dar de alta o algo asi pero en ingles xd</p>
+          <a href="./actAsignar.html">Asignación de Profesores</a><br />
+        </div>
+      </div>
+      <div class="col-md-8">
+        <div class="tab-content profile-tab" id="myTabContent">
+          <div
+            class="tab-pane fade show active"
+            id="home profesorProfile"
+            role="tabpanel"
+            aria-labelledby="home-tab"
+          >
+            <div class="row">
+              <div class="col-md-6">
+                <label>User Id</label>
+              </div>
+              <div class="col-md-6">
+                <p>${user.userID}</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <label>Name</label>
+              </div>
+              <div class="col-md-6">
+                <p>${user.fullName}</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <label>Email</label>
+              </div>
+              <div class="col-md-6">
+                <p>${user.email}</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <label>Profession</label>
+              </div>
+              <div class="col-md-6">
+                <p>Coordinadora de ${user.department}</p>
+              </div>
+            </div>
+          </div>
+  
+        </div>
+      </div>
+    </div>
+  </form>      
   </div>
-</form>
-    
-    
+
     `;
   }
 
-  if (currentUser.isAdmin) {
+  if (!user.isCoord && user.isAdmin) {
   }
 }
+
+loadUsers();
